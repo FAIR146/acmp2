58e0683ef0b327f5ff65079f990e288794e8c02a
FAIR146 <81648490+FAIR146@users.noreply.github.com>
2021-04-08 18:00:14 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HomeWork2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите заработные платы: минимальную, среднюю, высокую:");
+
+            int minimalPay = int.Parse(Console.ReadLine());
+            int midPay = int.Parse(Console.ReadLine());
+            int hightPay = int.Parse(Console.ReadLine());
+
+            int difference = minimalPay + midPay + hightPay;
+
+            difference = hightPay - minimalPay;
+
+            Console.WriteLine("Разница между минимальной и высокой зарплатой:");
+
+            Console.WriteLine(difference);
+
+            Console.ReadKey();         
+        }
+    }
+}
